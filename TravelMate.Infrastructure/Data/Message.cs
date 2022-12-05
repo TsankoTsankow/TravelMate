60c6ef28e77c0c87a75d9b1352d7c3858b635c6e
Tsanko Tsankov <ts.tsankow@gmail.com>
2022-12-05 11:55:56 +0200

    Adding Message Entity

---
+++ TravelMate.Infrastructure/Data/Message.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelMate.Infrastructure.Data
+{
+    public class Message
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string AuthorId { get; set; } = null!;
+
+        [Required]
+        [ForeignKey(nameof(AuthorId))]
+        public ApplicationUser Author { get; set; } = null!;
+
+        [Required]
+        public string ReceiverId { get; set; } = null!;
+
+        [Required]
+        [ForeignKey(nameof(AuthorId))]
+        public ApplicationUser Receiver { get; set; } = null!;
+
+        [Required]
+        public DateTime CreatedOn { get; set; }
+
+        public DateTime? ModifiedOn { get; set; }
+
+        public bool IsDeleted { get; set; } = false;
+    }
+}
