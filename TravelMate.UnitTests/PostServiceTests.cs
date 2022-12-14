b4ba587c82db16cc1b5c5596bf256feb20ef131c
Tsanko Tsankov <ts.tsankow@gmail.com>
2022-12-15 01:29:50 +0200

    Adding UnitTest project

---
+++ TravelMate.UnitTests/PostServiceTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelMate.UnitTests
+{
+    public class PostServiceTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+
+        }
+    }
+}
